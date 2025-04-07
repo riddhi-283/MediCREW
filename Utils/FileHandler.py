8c158654509925d130c2da9f994c774da8e4a562
riddhi-283 <lci2022040@iiitl.ac.in>
2025-04-07 21:48:16 +0530

    added report saving functionality

---
+++ Utils/FileHandler.py
@@ -0,0 +1,11 @@
+# Function to handle saving of files
+
+##-----------1---SAVE AS .TXT FILE--------------
+# import os
+# def save_report(role, report):
+#     folder = "Reports"
+#     os.makedirs(folder, exist_ok=True)
+#     with open(os.path.join(folder, f"{role}_report.txt"), "w", encoding="utf-8") as f:
+#         f.write(report)
+
+##------------2---SAVE AS .PDF FILE--------------
